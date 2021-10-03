f527df1848549aa3ba42541c4edfded32fb4badf
Joyfer <joyferramos@gmail.com>
2021-10-02 22:12:48 -0600

    Added transitions effects and loading component

--- src/components/reader/BookTextPage.jsx
+++ src/components/reader/BookTextPage.jsx
@@ -2,18 +2,42 @@ import React, { useContext } from "react";
 import PageReaderContext from "../../context/PageReaderContext";
 
 import Typography from "@mui/material/Typography";
+import CircularProgress from "@mui/material/CircularProgress";
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {},
+  loading: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100vh",
+  },
 }));
 
 const BookTextPage = () => {
   const { text, fontSize } = useContext(PageReaderContext);
+  const classes = useStyles();
+
   return (
-    <Typography variant="body1" style={{ fontSize: fontSize + "px" }}>
-      {text}
-    </Typography>
+    <>
+      {text != undefined ? (
+        <Typography
+          className="fadeIn first"
+          variant="body1"
+          style={{ fontSize: fontSize + "px" }}
+        >
+          {text === ""
+            ? "Nothing to preview, maybe PDF page is an image."
+            : text}
+        </Typography>
+      ) : (
+        <div className={classes.loading + " fadeIn first"}>
+          <CircularProgress></CircularProgress>
+        </div>
+      )}
+    </>
   );
 };
 
